39942eae6e7ea5f266562545433af72db7dd5d77
Abel B <abel.boilevin@biblibre.com>
2026-02-04 16:52:04 +0100

    Create past imports view with undo job.

---
+++ src/Job/UndoImportJob.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ClassicImporter\Job;
+
+use Omeka\Job\AbstractJob;
+use Omeka\Stdlib\Message;
+
+class UndoImportJob extends AbstractJob
+{
+    public function perform()
+    {
+        $services = $this->getServiceLocator();
+        $logger = $services->get('Omeka\Logger');
+        $api = $services->get('Omeka\ApiManager');
+
+        $jobId = $this->getArg('jobId');
+        $resources = $api->search('classicimporter_resource_maps', ['job_id' => intval($jobId)])->getContent();
+
+        if (!empty($resources)) {
+            foreach ($resources as $index => $resource) {
+                if ($this->shouldStop()) {
+                    $logger->warn(new Message(
+                        'The job "Undo" was stopped: %d/%d resources processed.', // @translate
+                        $index, count($resources)
+                    ));
+                    break;
+                }
+                try {
+                    $api->delete('classicimporter_resource_maps', $resource->id());
+                    switch ($resource->mappedResourceName()) {
+                        case 'item':
+                            $api->delete('items', $resource->resource()->id());
+                            break;
+                        case 'item_set':
+                            $api->delete('item_sets', $resource->resource()->id());
+                            break;
+                        case 'media':
+                            $api->delete('media', $resource->resource()->id());
+                            break;
+                        default:
+                            $logger->warn(sprintf('Invalid resource name: %s.', $resource->mappedResourceName())); // @translate
+                            break;
+                    }
+                } catch (\Exception $e) {
+                    $logger->warn(sprintf('Error when trying to delete resource: %s.', $e->getMessage())); // @translate
+                    // Nothing to do: already deleted.
+                }
+            }
+        }
+
+        else {
+            $logger->info('No resources found to undo.'); // @translate
+        }
+    }
+}
